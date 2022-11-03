00ce6263290efd2410350d2432ebfde0b3add4f5
ng <0xng@proton.me>
2022-11-03 19:43:46 +0200

    Show props in widget

--- widgets/middle.js
+++ widgets/middle.js
@@ -2,8 +2,8 @@
  * Created by: ng
  * Twitter: @iamng_eth
  *
- * Version: 0.2.0
- * Last Update: 26.10.2022
+ * Version: 0.1.0
+ * Last Update: 03.11.2022
  *
  * Enjoy!
  */
@@ -18,17 +18,17 @@ const greyOne = new Color('#BBBBBB')
 const greyTwo = new Color('#666666')
 const borderLight = new Color('#000000', 0.14)
 
-const coolBackground = '#d5d7e0'
-const coolBorder = '#bcc0d0'
-const coolDarkText = '#151c3b'
-const coolLightText = '#79809c'
-const coolAccent = '#e9ebf3'
+const coolBackground = new Color('#d5d7e0')
+const coolBorder = new Color('#bcc0d0')
+const coolDarkText = new Color('#151c3b')
+const coolLightText = new Color('#79809c')
+const coolAccent = new Color('#e9ebf3')
 
-const warmBackground = '#e2d7d5'
-const warmBorder = '#d3bcb9'
-const warmDarkText = '#221b1a'
-const warmLightText = '#8f7e7c'
-const warmAccent = '#f9f1f1'
+const warmBackground = new Color('#e2d7d5')
+const warmBorder = new Color('#d3bcb9')
+const warmDarkText = new Color('#221b1a')
+const warmLightText = new Color('#8f7e7c')
+const warmAccent = new Color('#f9f1f1')
 
 const widgetUrl = 'https://nouns.wtf/'
 
@@ -36,64 +36,63 @@ const data = await loadData()
 
 const seed = data.auction.seed
 
-const communityName = data.name
-const communityAuctions = data.auctions
+// const communityName = data.name
+// const communityAuctions = data.auctions
 
 const w = new ListWidget()
-w.backgroundColor = white
+w.backgroundColor = pickByState(coolBackground, warmBackground)
 w.url = widgetUrl
 
 const proposalsSectionTitleW = w.addStack()
 proposalsSectionTitleW.centerAlignContent()
 
 const propsSectionTitle = communityNameW.addText('Active Proposals')
-propsSectionTitle.textColor = black
+propsSectionTitle.textColor = pickByState(coolLightText, warmLightText)
 propsSectionTitle.font = Font.systemFont(12)
 
 proposalsSectionTitleW.addSpacer(6)
 
-proposalsSectionTitleW.addImage(createLine(600, 2, greyOne))
+proposalsSectionTitleW.addImage(createLine(600, 2, pickByState(coolBorder, warmBorder)))
 
 w.addSpacer(4)
 
-const now = new Date()
+// const now = Date.now()
+// const openAndVoting = []
+// const upcoming = []
 
-const openAndVoting = []
-const upcoming = []
-
-for (const auction of communityAuctions) {
-    if (auction.status === "Open" || auction.status === "Voting") {
-        openAndVoting.push(auction)
-    } else if (auction.status === "Upcoming") {
-        upcoming.push(auction)
-    }
-}
+// for (const auction of communityAuctions) {
+//     if (auction.status === "Open" || auction.status === "Voting") {
+//         openAndVoting.push(auction)
+//     } else if (auction.status === "Upcoming") {
+//         upcoming.push(auction)
+//     }
+// }
 
 let firstDone = false
 let totalDisplayed = 0
 
-for (const auction of openAndVoting) {
+for (const proposal of data.proposals) {
     if (totalDisplayed > 2) continue
     doSpacing()
-    displayAuction(auction)
+    displayProposal(proposal)
     totalDisplayed++
 }
 
-if (totalDisplayed <= 2) {
-    for (const auction of upcoming) {
-        if (totalDisplayed > 2) continue
-        doSpacing()
-        displayAuction(auction)
-        totalDisplayed++
-    }
-}
+// if (totalDisplayed <= 2) {
+//     for (const auction of upcoming) {
+//         if (totalDisplayed > 2) continue
+//         doSpacing()
+//         displayAuction(auction)
+//         totalDisplayed++
+//     }
+// }
 
-if (totalDisplayed == 0) {
-    w.addSpacer(6)
-    const noRounds = w.addText('No Active or Upcoming rounds')
-    noRounds.textColor = greyOne
-    noRounds.font = Font.systemFont(12)
-}
+// if (totalDisplayed == 0) {
+//     w.addSpacer(6)
+//     const noRounds = w.addText('No Active or Upcoming rounds')
+//     noRounds.textColor = greyOne
+//     noRounds.font = Font.systemFont(12)
+// }
 
 w.addSpacer(null)
 
@@ -118,33 +117,32 @@ function doSpacing() {
     }
 }
 
-function displayAuction(auction) {
+function displayProposal(proposal) {
     let barTextColor
     let barBorderColor
     let barText
     let time
 
-    if (auction.status === "Open") {
-        barTextColor = green
-        barBorderColor = greenSemiTransparent
-        barText = 'Proposing'
-        time = new Date(auction.proposalEndTime)
-    } else if (auction.status === "Voting") {
-        barTextColor = purple
-        barBorderColor = purpleSemiTransparent
-        barText = 'Voting'
-        time = new Date(auction.votingEndTime)
-    } else if (auction.status === "Upcoming") {
-        barTextColor = greyTwo
-        barBorderColor = borderLight
-        barText = 'Not Started'
-        time = new Date(auction.startTime)
+    if (proposal.state === "ACTIVE") {
+        barTextColor = pickByState(coolLightText, warmLightText)
+        barBorderColor = pickByState(coolBorder, warmBorder)
+        barText = 'Active'
+        time = new Date(proposal.endTime)
     }
+    // else if (auction.status === "Voting") {
+    //     barTextColor = purple
+    //     barBorderColor = purpleSemiTransparent
+    //     barText = 'Voting'
+    //     time = new Date(auction.votingEndTime)
+    // } else if (auction.status === "Upcoming") {
+    //     barTextColor = greyTwo
+    //     barBorderColor = borderLight
+    //     barText = 'Not Started'
+    //     time = new Date(auction.startTime)
+    // }
 
     const deadline = getTime(time)
-    const funding = `${auction.fundingAmount} ${auction.currencyType} × ${auction.numWinners}`
-    const proposals = auction.proposals.length
-    const title = auction.title
+    const title = proposal.title
 
     const titleW = w.addStack()
     titleW.centerAlignContent()
@@ -155,48 +153,49 @@ function displayAuction(auction) {
     barW.borderColor = barBorderColor
     barW.setPadding(2, 3, 2, 3)
 
-    const barTxt = barW.addText(barText)
+    // const barTxt = barW.addText(barText)
+    const barTxt = barW.addText(deadline)
     barTxt.textColor = barTextColor
     barTxt.font = Font.boldSystemFont(8)
 
     titleW.addSpacer(4)
 
     const titleText = titleW.addText(title)
-    titleText.textColor = black
+    titleText.textColor = pickByState(coolDarkText, warmDarkText)
     titleText.font = Font.semiboldSystemFont(12)
 
-    const infoW = w.addStack()
+    // const infoW = w.addStack()
 
-    const fundingInfoText = infoW.addText('Funding ')
-    fundingInfoText.textColor = greyOne
-    fundingInfoText.font = Font.mediumSystemFont(10)
+    // const fundingInfoText = infoW.addText('Funding ')
+    // fundingInfoText.textColor = greyOne
+    // fundingInfoText.font = Font.mediumSystemFont(10)
 
-    const fundingText = infoW.addText(funding)
-    fundingText.textColor = black
-    fundingText.font = Font.mediumSystemFont(10)
+    // const fundingText = infoW.addText(funding)
+    // fundingText.textColor = black
+    // fundingText.font = Font.mediumSystemFont(10)
 
-    infoW.addSpacer(16)
+    // infoW.addSpacer(16)
 
-    const deadlineInfoTxt = auction.status === "Upcoming" ? 'Starts ' : 'Deadline '
-    const deadlineInfoText = infoW.addText(deadlineInfoTxt)
-    deadlineInfoText.textColor = greyOne
-    deadlineInfoText.font = Font.mediumSystemFont(10)
+    // const deadlineInfoTxt = auction.status === "Upcoming" ? 'Starts ' : 'Deadline '
+    // const deadlineInfoText = infoW.addText(deadlineInfoTxt)
+    // deadlineInfoText.textColor = greyOne
+    // deadlineInfoText.font = Font.mediumSystemFont(10)
 
-    const deadlineText = infoW.addText(deadline)
-    deadlineText.textColor = black
-    deadlineText.font = Font.mediumSystemFont(10)
+    // const deadlineText = infoW.addText(deadline)
+    // deadlineText.textColor = black
+    // deadlineText.font = Font.mediumSystemFont(10)
 
-    if (auction.status !== "Upcoming") {
-        infoW.addSpacer(16)
+    // if (auction.status !== "Upcoming") {
+    //     infoW.addSpacer(16)
 
-        const proposalsInfoText = infoW.addText('Proposals ')
-        proposalsInfoText.textColor = greyOne
-        proposalsInfoText.font = Font.mediumSystemFont(10)
+    //     const proposalsInfoText = infoW.addText('Proposals ')
+    //     proposalsInfoText.textColor = greyOne
+    //     proposalsInfoText.font = Font.mediumSystemFont(10)
 
-        const proposalsText = infoW.addText(`${proposals}`)
-        proposalsText.textColor = black
-        proposalsText.font = Font.mediumSystemFont(10)
-    }
+    //     const proposalsText = infoW.addText(`${proposals}`)
+    //     proposalsText.textColor = black
+    //     proposalsText.font = Font.mediumSystemFont(10)
+    // }
 }
 
 async function loadImage(imageUrl) {
