a4453ab1737528c94818c707be349d3d59c96364
ng <0xng@proton.me>
2022-11-07 19:35:45 +0200

    Fix ens shorting issues

--- backend/source/utils/addressAndENSDisplayUtils.ts
+++ backend/source/utils/addressAndENSDisplayUtils.ts
@@ -1,18 +1,5 @@
-export const veryShortENS = (ens: string) => {
-  return [ens.substring(0, 1), ens.substring(ens.length - 3, ens.length)].join(
-    '...'
-  )
-}
-
-export const veryShortAddress = (address: string) => {
-  return [
-    address.substring(0, 3),
-    address.substring(address.length - 1, address.length)
-  ].join('...')
-}
-
 export const shortENS = (ens: string) => {
-  if (ens.length < 15 || window.innerWidth > 480) {
+  if (ens.length < 15) {
     return ens
   }
   return [ens.substring(0, 4), ens.substring(ens.length - 8, ens.length)].join(
