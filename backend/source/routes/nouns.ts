f2933e1732e450877433b4996ebaa3d3fa6685f4
ng <0xng@proton.me>
2022-11-27 22:26:14 +0200

    Backend and basic widget

--- backend/source/routes/nouns.ts
+++ backend/source/routes/nouns.ts
@@ -1,7 +1,10 @@
 import express from 'express'
-import controller from '../controllers/nouns'
+import nounsController from '../controllers/nouns'
+import lilNounsController from '../controllers/lilnouns'
+
 const router = express.Router()
 
-router.get('/nouns', controller.getNounsData)
+router.get('/nouns', nounsController.getNounsData)
+router.get('/lil-nouns', lilNounsController.getLilNounsData)
 
 export = router
