9d4ca7b29ca40441768195d3e37ab1a71be6c15b
ng <0xng@proton.me>
2022-12-12 21:58:02 +0200

    Lock screen widget

---
+++ widgets/nouns/middle-lock-screen.js
@@ -0,0 +1,84 @@
+/*
+ * Created by: ng
+ * Twitter: @iamng_eth
+ *
+ * Version: 0.1.0
+ * Last Update: 12.12.2022
+ *
+ * Enjoy!
+ */
+
+const data = await loadData()
+
+const widgetUrl = 'https://nouns.wtf/'
+
+const id = data.auction.id
+const currentBid = data.auction.currentBid
+const endTime = data.auction.endTime
+
+const w = new ListWidget()
+w.url = widgetUrl
+
+const nounW = w.addStack()
+nounW.layoutHorizontally()
+
+nounW.addText(`Noun ${id}`)
+nounW.font = Font.boldSystemFont(12)
+nounW.addSpacer(null)
+nounW.addText(`${currentBid} Ξ`)
+
+const auctionW = w.addStack()
+auctionW.layoutHorizontally()
+
+auctionW.addText(`Ends In`)
+auctionW.font = Font.boldSystemFont(12)
+auctionW.addSpacer(null)
+const now = new Date().valueOf()
+const timeToGo = endTime - now
+const timeToGoText = secondsToDhms(timeToGo / 1000)
+auctionW.addText(timeToGoText)
+
+const propsW = w.addStack()
+propsW.layoutHorizontally()
+
+propsW.addText(`Active Props`)
+propsW.font = Font.boldSystemFont(12)
+propsW.addSpacer(null)
+propsW.addText(`${data.proposals.length}`)
+
+w.addSpacer(null)
+
+Script.setWidget(w)
+Script.complete()
+w.presentMedium()
+
+/*
+* Utility Functions
+*/
+
+async function loadData() {
+    console.log('loading data')
+    const url = 'https://api.nounswidgets.wtf/nouns'
+    const req = new Request(url)
+    req.method = 'GET'
+    req.headers = { 'Content-Type': 'application/json' }
+    const res = await req.loadJSON()
+
+    console.log(res)
+
+    return res
+}
+
+function secondsToDhms(seconds) {
+    seconds = Number(seconds)
+
+    var h = Math.floor((seconds % (3600 * 24)) / 3600)
+    var m = Math.floor((seconds % 3600) / 60)
+    var s = Math.floor(seconds % 60)
+
+    var hDisplay = h > 0 ? h + 'h ' : ''
+    var mDisplay = m > 0 ? m + 'm ' : ''
+    var sDisplay = h > 0 ? s > 0 ? s + 's' : '' : ''
+
+    return hDisplay + mDisplay + sDisplay
+}
