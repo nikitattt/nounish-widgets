bdd0c5d4e6b7d742aadbdeb22609f55b4ee921e0
ng <0xng@proton.me>
2023-01-03 21:32:20 +0200

    Add more props data for nouns

--- backend/source/utils/types.ts
+++ backend/source/utils/types.ts
@@ -1,35 +1,42 @@
-export interface Proposal {
-  title: string
-  state: string
-  endTime: number
-}
+// export interface Proposal {
+//   id: number
+//   title: string
+//   state: string
+//   endTime: number
+//   quorum?: number
+//   votes?: {
+//     yes: number
+//     no: number
+//     abstain: number
+//   }
+// }
 
-export type Seed = {
-  head: number
-  glasses: number
-  body: number
-  accessory: number
-  background: number
-}
+// export type Seed = {
+//   head: number
+//   glasses: number
+//   body: number
+//   accessory: number
+//   background: number
+// }
 
-export interface Auction {
-  id: number
-  currentBid: String
-  bidder: String
-  endTime: number
-  image: String
-  seed: Seed
-}
+// export interface Auction {
+//   id: number
+//   currentBid: String
+//   bidder: String
+//   endTime: number
+//   image: String
+//   seed: Seed
+// }
 
-export interface Nouns {
-  auction: Auction
-  proposals?: Proposal[]
-}
+// export interface Nouns {
+//   auction: Auction
+//   proposals?: Proposal[]
+// }
 
-export interface ProposalSubgraphEntity {
-  id: String
-  startBlock: string
-  endBlock: string
-  status: string
-  description: string
-}
+// export interface ProposalSubgraphEntity {
+//   id: String
+//   startBlock: string
+//   endBlock: string
+//   status: string
+//   description: string
+// }
