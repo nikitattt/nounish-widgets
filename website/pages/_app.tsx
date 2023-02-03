384a113f689e211aa3964818dfbf564380fab199
ng <0xng@proton.me>
2023-02-03 15:22:09 +0200

    Add iamng website copy

---
+++ website/pages/_app.tsx
@@ -0,0 +1,22 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import ReactTooltip from 'react-tooltip'
+import Script from "next/script";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <>
+    <Component {...pageProps} />
+    <ReactTooltip type="info" event="custom" />
+    <Script src="https://adata.iamng.wtf/latest.js" />
+    <noscript>
+      {/* eslint-disable @next/next/no-img-element */}
+      <img
+        src="https://adata.iamng.wtf/noscript.gif"
+        alt=""
+        referrerPolicy="no-referrer-when-downgrade"
+      />
+    </noscript>
+  </>
+}
+
+export default MyApp
