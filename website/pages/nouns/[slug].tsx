3cfb16edbac5abe12ab1a7f79e132b7dadadbc31
ng <0xng@proton.me>
2023-02-22 01:47:39 +0200

    Add test script add link

--- website/pages/nouns/[slug].tsx
+++ website/pages/nouns/[slug].tsx
@@ -127,6 +127,13 @@ const NounsWidgetPage: NextPage<{ data: any }> = (props) => {
           >
             <p className="font-bold">Download</p>
           </button>
+          {/* scriptable:///run?scriptName=ScriptDude&name=COVID%20Trend%20Widget&source=https://github.com/mvan231/Scriptable/blob/main/COVID%20Trend%20Widget/COVID%20Trend%20Widget.js&docs=https://github.com/mvan231/Scriptable/tree/main/COVID%20Trend%20Widget */}
+          <a
+            className="cursor-pointer px-12 py-5 bg-blue rounded-2xl hover:bg-white hover:border-blue border-4 text-white  hover:text-blue"
+            href={`scriptable:///run?scriptName=${'WidgetTest'}&name=${'WidgetTestName'}&source=${'https://github.com/mvan231/Scriptable/blob/main/COVID%20Trend%20Widget/COVID%20Trend%20Widget.js'}&docs=${'https://github.com/mvan231/Scriptable/tree/main/COVID%20Trend%20Widget'}`}
+          >
+            <p className="font-bold">Save to Scriptable</p>
+          </a>
         </div>
         {/* <div className="mt-2 flex flex-row gap-1 items-baseline justify-center text-sm">
                     <a
